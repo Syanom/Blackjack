30b7a65e0568e3acdeb24f21b639df972dff23d9
Syanom <yandupliy@gmail.com>
2021-10-07 23:47:35 +0300

    make another_game? method

--- game_manager.rb
+++ game_manager.rb
@@ -10,5 +10,25 @@ class GameManager
     @players << Player.new << Dealer.new
   end
 
-  def start_game; end
+  def start_game
+    loop do
+      another_game?
+    end
+  end
+
+  def another_game?
+    print 'Do you want another game? y/n: '
+    case gets.chomp
+    when 'y'
+      return_cards
+      next
+    when 'n'
+      break
+    else
+      raise 'Wrong command'
+    end
+  rescue RuntimeError => e
+    puts e.message
+    retry
+  end
 end
