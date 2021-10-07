3f2f502672d47d29e9391004322335a3fb75bff6
Syanom <yandupliy@gmail.com>
2021-10-07 14:41:52 +0300

    create card class

---
+++ card.rb
@@ -0,0 +1,25 @@
+# Cards for the game
+class Card
+  attr_reader :value, :rank, :suit
+
+  def initialize(rank, suit)
+    @suit = suit
+    @rank = rank
+    validate!
+    @value = VALUES_BY_RANKS[rank]
+  end
+
+  protected
+
+  VALUES_BY_RANKS = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10,
+                      'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11 }.freeze
+
+  SUITS = ['+', '<3', '^', '<>'].freeze
+
+  def validate!
+    errors = []
+    errors << "Card's rank: #{rank} is invalid" if VALUES_BY_RANKS[rank].nil?
+    errors << "Card's suit is invalid: #{suit}" unless SUITS.include?(suit)
+    raise errors.join('. ') unless errors.empty?
+  end
+end
