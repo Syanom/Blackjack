7d84f2ae2d092cc6fb29ead1fab6e493156b2d5b
Syanom <yandupliy@gmail.com>
2021-10-07 21:36:39 +0300

    finish Hand class

--- deck.rb
+++ deck.rb
@@ -8,4 +8,41 @@ class Deck
       Card.suits.each { |suit| @cards << Card.new(rank, suit) }
     end
   end
+
+  def shuffle
+    cards.shuffle!
+  end
+
+  def return_cards(*cards_to_return)
+    validate_cards!(*cards_to_return)
+    cards.push(*cards_to_return)
+  end
+
+  def give_cards(amount)
+    result = []
+    amount.times do
+      result << cards.shift
+    end
+    result
+  end
+
+  protected
+
+  def validate_cards!(*cards_to_validate)
+    errors = []
+    cards_to_validate.each do |card|
+      errors << 'Given card is invalid' unless card.is_a?(Card)
+      errors << 'Card is in deck already' if include_card?(card)
+    end
+    raise errors.join('. ') unless errors.empty?
+  end
+
+  def include_card?(card)
+    cards.each do |card_in_deck|
+      return true if card_in_deck.rank == card.rank && card_in_deck.suit == card.suit
+    end
+    false
+  end
+
+  attr_accessor :cards
 end
---
+++ hand.rb
@@ -0,0 +1,46 @@
+require_relative 'card'
+
+# Hands of our players
+class Hand
+  attr_accessor :cash
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+    @cash = 100
+  end
+
+  def count_points
+    ace = false
+    points = 0
+    cards.each do |card|
+      if card.rank == 'A'
+        ace = true
+        next
+      end
+      points += card.value
+    end
+    return points if ace == false
+
+    points += points + 11 <= 21 ? 11 : 1
+  end
+
+  def drop_cards
+    result = []
+    result.push(*cards)
+    cards.clear
+    result
+  end
+
+  def take_cards(*cards_to_take)
+    cards.push(*cards_to_take)
+  end
+
+  def draw
+    result = ''
+    cards.each do |card|
+      result += "#{card.draw} "
+    end
+    result += "                     #{cash}"
+  end
+end
