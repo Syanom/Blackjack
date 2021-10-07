ef753ff3abea7c6a29c8c786f7f9297719d30e29
Syanom <yandupliy@gmail.com>
2021-10-07 14:52:19 +0300

    add draw method to Card

--- card.rb
+++ card.rb
@@ -9,6 +9,16 @@ class Card
     @value = VALUES_BY_RANKS[rank]
   end
 
+  def draw
+    card = "[#{rank}"
+    card + case suit
+           when '+', '^'
+             " #{suit}]"
+           else
+             "#{suit}]"
+           end
+  end
+
   protected
 
   VALUES_BY_RANKS = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10,
